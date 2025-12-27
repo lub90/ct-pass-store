c141021a87c6352f795eb690569dc142cfd05c86
Lukas Block <61112699+lub90@users.noreply.github.com>
2025-12-27 20:23:42 +0000

    Exported reusable components into separate repo

--- extension/src/types/AlertState.ts
+++
@@ -1,8 +0,0 @@
-export const AlertState = {
-  Idle: "idle",
-  Loading: "loading",
-  Success: "success",
-  Error: "error",
-} as const;
-
-export type AlertState = typeof AlertState[keyof typeof AlertState];
--- extension/src/types/ViewState.ts
+++
@@ -1,7 +0,0 @@
-export const ViewState = {
-  Loading: "loading",
-  Ready: "ready",
-  Error: "error",
-} as const;
-
-export type ViewState = typeof ViewState[keyof typeof ViewState];
